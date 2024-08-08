90e4500ff1486b3fb69e55a03135419df33afdec
Stancu Florin <niflostancu@gmail.com>
2024-08-08 14:33:37 +0300

    uboot: override default environment

--- scripts/uboot.mk
+++ scripts/uboot.mk
@@ -16,9 +16,10 @@ UBOOT_MAKE_FLAGS = $(_XC_ARG) -j "$(NPROC)"
 # uncomment for make script debugging
 #UBOOT_MAKE_FLAGS += V=1
 UBOOT_EXTRA_CONFIGS ?= $(SRC)/configs/uboot-imx8mq.config
-UBOOT_ENV_SOURCE_FILE=uboot-custom.env
-UBOOT_ENV_SOURCE_FILE_DEST=$(UBOOT_DIR)/board/technexion/pico-imx8mq/$(UBOOT_ENV_SOURCE_FILE)
 
+UBOOT_DEFAULT_ENV_FILE ?= $(SRC)/configs/uboot-default.env
+UBOOT_ENV_OVERLAY_CONFIG = $(UBOOT_DIR)/env-overlay.config
+UBOOT_EXTRA_CONFIGS += $(UBOOT_ENV_OVERLAY_CONFIG)
 
 .PHONY: uboot uboot_clean
 _UBOOT_COMPILED_TARGET = $(UBOOT_DIR)/.uboot-compiled
@@ -29,8 +30,7 @@ $(UBOOT_DIR)/.git:
 	git clone "$(UBOOT_GIT_URL)" --branch="$(UBOOT_GIT_BRANCH)" "$(UBOOT_DIR)"
 
 $(UBOOT_GENERATED_FILES_FULL): $(_UBOOT_COMPILED_TARGET)
-$(_UBOOT_COMPILED_TARGET): $(UBOOT_DIR)/.config $(UBOOT_EXTRA_PATCH)  \
-		$(UBOOT_ENV_SOURCE_FILE_DEST) $(_FORCE)
+$(_UBOOT_COMPILED_TARGET): $(UBOOT_DIR)/.config $(UBOOT_EXTRA_PATCH) $(_FORCE)
 	if [[ -n "$(UBOOT_EXTRA_PATCH)" ]]; then \
 		cd "$(UBOOT_DIR)/" && \
 		if ! patch -R -p1 -s -f --dry-run < "$(UBOOT_EXTRA_PATCH)"; then \
@@ -50,8 +50,14 @@ $(UBOOT_DIR)/.config: $(UBOOT_DIR)/.extraconfig
 $(UBOOT_DIR)/.extraconfig: $(UBOOT_EXTRA_CONFIGS) | $(UBOOT_DIR)/.git
 	echo | cat $(UBOOT_EXTRA_CONFIGS) > "$@"
 
-$(UBOOT_ENV_SOURCE_FILE_DEST): $(UBOOT_ENV_SOURCE_FILE)
-	cp -f "$<" "$@"
+$(UBOOT_ENV_OVERLAY_CONFIG): $(UBOOT_DEFAULT_ENV_FILE) | $(UBOOT_DIR)/.git
+	echo "$$_UBOOT_ENV_OVERLAY_" > $@
+define _UBOOT_ENV_OVERLAY_=
+CONFIG_USE_DEFAULT_ENV_FILE=y
+CONFIG_DEFAULT_ENV_FILE="$(UBOOT_DEFAULT_ENV_FILE)"
+
+endef
+export _UBOOT_ENV_OVERLAY_
 
 uboot_menuconfig:
 	$(MAKE) -C $(UBOOT_DIR) $(UBOOT_MAKE_FLAGS) menuconfig
