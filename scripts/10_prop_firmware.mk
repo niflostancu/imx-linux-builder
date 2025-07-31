c65486d2d11b4405807c313b7d0122a33bc7042a
Stancu Florin <niflostancu@gmail.com>
2025-07-31 16:37:40 +0300

    prop_firmware: imx sentinel download script
    (required for imx9*)

--- scripts/10_prop_firmware.mk
+++ scripts/10_prop_firmware.mk
@@ -3,14 +3,26 @@
 
 # i.MX firmware blobs (downloaded)
 IMX_FW_DEST ?= $(BUILD_DEST)/firmware
-IMX_FW_VER ?= 8.22
 IMX_FW_URL ?= http://sources.buildroot.net/firmware-imx/firmware-imx-$(IMX_FW_VER).bin
+IMX_FW_VER ?= 8.22
 IMX_FW_BIN ?= firmware-imx-$(IMX_FW_VER).bin
 IMX_FW_EXTRACT_DIR ?= $(IMX_FW_DEST)/firmware-imx-$(IMX_FW_VER)
+
+# IMX Sentinel Firmware for iMX >= 9
+# From: https://docs.u-boot.org/en/latest/board/nxp/imx93_frdm.html
+IMX_SENTINEL_URL ?= https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-sentinel-$(IMX_SENTINEL_VER).bin
+IMX_SENTINEL_VER ?= 0.11
+IMX_SENTINEL_BIN ?= firmware-sentinel-$(IMX_SENTINEL_VER).bin
+IMX_SENTINEL_EXTRACT_DIR ?= $(IMX_FW_DEST)/firmware-sentinel-$(IMX_SENTINEL_VER)
+
 # full path to the extracted firmware files to be included
 IMX_FW_BIN_FILES_FULL ?=
+IMX_SENTINEL_FILES_FULL ?=
 # ... and their relative paths:
-IMX_FW_BIN_FILES = $(IMX_FW_BIN_FILES_FULL:$(IMX_FW_EXTRACT_DIR)/%=%)
+IMX_FW_BIN_FILES ?=
+IMX_FW_BIN_FILES += \
+		$(IMX_FW_BIN_FILES_FULL:$(IMX_FW_EXTRACT_DIR)/%=%) \
+		$(IMX_SENTINEL_FILES_FULL:$(IMX_SENTINEL_EXTRACT_DIR)/%=%)
 
 # use a dummy extraction target due to multiple artifacts being produced
 _IMX_FW_EXTRACTED_TARGET = $(IMX_FW_EXTRACT_DIR)/.extracted
@@ -35,3 +47,22 @@ imx_fw_clean:
 
 all: imx_fw
 clean_all: imx_fw_clean
+
+# i.MX Sentinel Firmware download target
+_IMX_SENTINEL_EXTRACTED_TARGET = $(IMX_SENTINEL_EXTRACT_DIR)/.extracted
+$(IMX_SENTINEL_BIN_FILES_FULL): $(_IMX_SENTINEL_EXTRACTED_TARGET)
+.PHONY: imx_sentinel imx_sentinel_clean
+imx_sentinel:
+	$(MAKE_FORCED) $(_IMX_SENTINEL_EXTRACTED_TARGET)
+$(_IMX_SENTINEL_EXTRACTED_TARGET):
+	mkdir -p "$(IMX_FW_DEST)"
+	# download sentinel bin file from HTTP (using CLI tool wget)
+	[[ -f "$(IMX_FW_DEST)/$(IMX_SENTINEL_BIN)" ]] || \
+		wget $(IMX_SENTINEL_URL) -O "$(IMX_FW_DEST)/$(IMX_SENTINEL_BIN)"
+	chmod +x "$(IMX_FW_DEST)/$(IMX_SENTINEL_BIN)"
+	# extract sentinel if not already (auto-accept license)
+	[[ -d "$(IMX_SENTINEL_EXTRACT_DIR)" ]] || ( \
+		cd "$(IMX_FW_DEST)" && ./$(IMX_SENTINEL_BIN) --auto-accept )
+	# finally, create a dummy file to mark target as done
+	touch "$@"
+
